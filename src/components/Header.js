3e80714436581d2630fcf12a633ccb48ccbaf836
Taylor Johnson <tjohnson@metrocommercial.com>
2018-09-19 13:53:41 -0400

    make header component and stylesheet

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/Header.css';
+
+class Header extends Component {
+    render() {
+      return (
+        <header className="App-header">
+            <h1 className="App-title">Swole</h1>
+        </header>
+      );
+    }
+  }
+
+export default Header;

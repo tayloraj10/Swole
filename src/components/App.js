8c88a75ebdf87b2564a37de3f9a2ebf69bd4db39
Taylor Johnson <tjohnson@metrocommercial.com>
2018-09-19 17:00:43 -0400

    work on spreadsheet component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
+
 import Header from './Header';
+import Spreadsheet from './Spreadsheet';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-        </p>
+        <div className='Spreadsheet'>
+          <Spreadsheet />
+        </div>
       </div>
     );
   }

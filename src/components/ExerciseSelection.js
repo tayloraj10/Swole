a8e501d7cc2819ab8c8b12edef924bc33b058f29
Taylor Johnson <tjohnson@metrocommercial.com>
2019-03-15 10:17:12 -0400

    be like rob 2

--- src/components/ExerciseSelection.js
+++ src/components/ExerciseSelection.js
@@ -3,16 +3,18 @@ import React, { Component } from "react";
 class ExerciseSelection extends Component {
   render() {
     return (
-      {props.uniqueExercises && 
-      <select onChange={this.props.change}>
-        <option>{"Pick an Exercise"}</option>
-        {this.props.uniqueExercises.map((x, y) => (
-          <option key={y} value={x}>
-            {x}
-          </option>
-        ))}
-      </select>
-    }
+      <div>
+        {this.props.uniqueExercises && (
+          <select onChange={this.props.change}>
+            <option>{"Pick an Exercise"}</option>
+            {this.props.uniqueExercises.map((x, y) => (
+              <option key={y} value={x}>
+                {x}
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
     );
   }
 }

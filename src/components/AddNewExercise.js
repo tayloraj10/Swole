4937a1ca2103d4d8f2ab290a5c4fbe7cf5c34241
Taylor Johnson <tjohnson@metrocommercial.com>
2019-03-15 13:27:36 -0400

    add new exercise component and install material ui

---
+++ src/components/AddNewExercise.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class AddNewExercise extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default AddNewExercise;
